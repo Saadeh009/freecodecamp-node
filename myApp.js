ffb05b4168b985c205607429db6c57db1b59b3bc
Saadeh009 <saadehelias9@gmail.com>
2023-10-14 10:43:33 +0300

    post request

--- myApp.js
+++ myApp.js
@@ -46,6 +46,11 @@ app.get("/name", (req, res) => {
     res.json({name: `${first} ${last}`})
 })
 
+app.post("/name", (req, res) => {
+    let {first, last} = req.body
+    res.json({name: `${first} ${last}`})
+})
+
 
 
 
